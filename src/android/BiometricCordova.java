afc5d0a64fe74877513f70f63545133d6a552722
Camilo Vega <camilo_25_00@hotmail.com>
2025-07-13 01:37:18 -0500

    cambio import

--- src/android/BiometricCordova.java
+++ src/android/BiometricCordova.java
@@ -8,8 +8,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import MainActivity;
-import ScanActionCryptoActivity;
+import biometric.entel.MainActivity;
+import biometric.entel.ScanActionCryptoActivity;
 
 
 
