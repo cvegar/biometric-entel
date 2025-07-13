d4b9694e39314678776ca1f74769ec3ef87ba221
Camilo Vega <camilo_25_00@hotmail.com>
2025-07-13 12:28:54 -0500

    subiendo plugin

--- www/BiometricCordova.js
+++ www/BiometricCordova.js
@@ -1,13 +1,23 @@
-var exec = require('cordova/exec');
+function BiometricCordova() {}
 
-var BiometricCordova = {
-    launchMainActivity: function(successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "BiometricCordova", "launchMainActivity", []);
-    },
-
-    launchScanCrypto: function(successCallback, errorCallback) {
-        exec(successCallback, errorCallback, "BiometricCordova", "launchScanCrypto", []);
-    }
+// Método para lanzar MainActivity
+BiometricCordova.prototype.launchMainActivity = function(successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'BiometricCordova', 'launchMainActivity', []);
 };
 
-module.exports = BiometricCordova;
+// Método para lanzar ScanActionCryptoActivity
+BiometricCordova.prototype.launchScanCrypto = function(successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'BiometricCordova', 'launchScanCrypto', []);
+};
+
+// Función de instalación
+BiometricCordova.install = function() {
+    if (!window.plugins) {
+        window.plugins = {};
+    }
+    window.plugins.BiometricCordova = new BiometricCordova();
+    return window.plugins.BiometricCordova;
+};
+
+// Registro del constructor
+cordova.addConstructor(BiometricCordova.install);
